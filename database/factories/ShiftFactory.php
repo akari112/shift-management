3048979b5f504e62f3fd7ad4ab32f33e126ef2c8
大石　あかり <65846922+akari112@users.noreply.github.com>
2020-11-24 02:03:35 +0900

    Add files via upload

--- database/factories/ShiftFactory.php
+++ database/factories/ShiftFactory.php
@@ -6,7 +6,14 @@ use App\Shift;
 use Faker\Generator as Faker;
 
 $factory->define(Shift::class, function (Faker $faker) {
+    static $seed = 0;
+
+    $faker->seed($seed++);
+
     return [
-        //
+        'user_id' => factory(App\User::class),
+        'day' => $faker->dateTimeBetween('-30day', '+60day'),
+        'desired' => $faker->boolean,
+        'confirm' => $faker->boolean,
     ];
 });
