8b848d4973883c6027742c6fa908319903e71691
大石　あかり <65846922+akari112@users.noreply.github.com>
2020-11-24 02:08:47 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,6 @@
 
 <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <!-- Font Awesome -->
 <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
 @yield('script')
